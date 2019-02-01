b287a7cf1c5bd4a4a54be12c6655411480a36850
anderson <deveil2825@163.com>
2019-02-01 14:05:53 +0800

    修改swjs
    修改swjs

--- service-demo/sw.js
+++ service-demo/sw.js
@@ -1,50 +1,30 @@
-if('serviceWorker' in navigator){
-	window.addEventListener('load',function() {
-		navigator.serviceWorker.register('sw.js',{scope:''})
-		.then(function(registration){
-
-			//注册成功
-			console.log('serviceWorker registration successful with scope:',registration.scope);
-		})
-		.catch(function(err){
-			//注册失败
-			console.log('serviceWorke registration successful with scope:',err);
-		});
-	});
 	this.addEventListener('install',function (event){
 		event.waitUntil(
 			caches.open('my-test-cache-v1').then(function (cache){
 				return cache.addAll([
-					'/',
-					'/index.html',
-	                '/main.css',
-	                '/main.js',
-	                '/images.jpg'
-				])
+					'/service-demo/',
+					'/service-demo/index.html',
+	                '/service-demo/main.css',
+	                '/service-demo/main.js',
+	                '/service-demo/images.jpg'
+				]);
 			})		
-		)
+		);
 	});
 	this.addEventListener('fetch',function (event) {
 		event.respondWith(
 			caches.match(event.request).then(function (response){
-				if(response) {
+				if(response !== undefined) {
 					return response;
-				}
-
-				var request = event.request.clone();
- 				return fetch(request).then(function (httpRes){
- 					if(!httpRes || httpRes.status !== 200){
- 						return httpRes;
- 					}
-
- 					var responseClone = httpRes.clone();
- 					caches.open('my-test-cache-v1').then(function (cache) {
- 						cache.put(event.request,responseClone);
- 					});
-
- 					return httpRes;
-				})		
-			})
+				}else{
+					return fetch(event.request).then(function (response){
+	 				 	var responseClone = response.clone();
+	 					caches.open('my-test-cache-v1').then(function (cache) {
+	 						cache.put(event.request,responseClone);
+	 					});
+ 						return response;
+					}); 	
+ 				}
+ 			});
 		)	
-	})
-}
+	});
