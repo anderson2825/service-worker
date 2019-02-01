e6a55d1eedf66a91e3fb1e2aac22d7e69c05bc95
anderson <deveil2825@163.com>
2019-02-01 10:51:41 +0800

    swjs对象改成数组
    swjs对象改成数组

--- service-demo/sw.js
+++ service-demo/sw.js
@@ -14,13 +14,13 @@ if('serviceWorker' in navigator){
 	this.addEventListener('install',function (event){
 		event.waitUntil(
 			caches.open('my-test-cache-v1').then(function (cache){
-				return cache.addAll({
+				return cache.addAll([
 					'/',
 					'/index.html',
 	                '/main.css',
 	                '/main.js',
 	                '/images.jpg'
-				})
+				])
 			})		
 		)
 	});
