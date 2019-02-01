e73d162d253ba3c7aac16200fab93b3152e88d33
anderson <deveil2825@163.com>
2019-02-01 10:37:00 +0800

    新增测试demo
    新增测试demo

--- service-demo/sw.js
+++ service-demo/sw.js
@@ -10,5 +10,41 @@ if('serviceWorker' in navigator){
 			//注册失败
 			console.log('serviceWorke registration successful with scope:',err);
 		});
+	});
+	this.addEventListener('install',function (){
+		event.waitUntil(
+			caches.open('my-test-cache-v1').then(function (cache){
+				return cache.addAll({
+					'/',
+	                '/index.html',
+	                '/main.css',
+	                '/main.js',
+	                '/images.jpg'
+				})
+			})		
+		)
+	});
+	this.addEventListener('fetch',function (event) {
+		event.respondWith(
+			caches.match(event.request).then(function (response){
+				if(response) {
+					return response;
+				}
+
+				var request = event.request.clone();
+ 				return fetch(request).then(function (httpRes){
+ 					if(!httpRes || httpRes.status !== 200){
+ 						return httpRes;
+ 					}
+
+ 					var responseClone = httpRes.clone();
+ 					caches.open('my-test-cache-v1').then(function (cache) {
+ 						cache.put(event.request,responseClone);
+ 					});
+
+ 					return httpRes;
+				})		
+			})
+		)	
 	})
 }
