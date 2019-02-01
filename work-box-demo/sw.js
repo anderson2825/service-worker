f0267244dcbf983237fa455f222acf6ac8a059a0
anderson <deveil2825@163.com>
2019-02-01 15:49:43 +0800

    index修改成html模式
    index修改成html模式

--- work-box-demo/sw.js
+++ work-box-demo/sw.js
@@ -1,5 +1,4 @@
 console.log('hello from sw.js');
-// workbox 2.x 是将 workbox 核心内容放在 workbox-sw node_modules 包里维护的
 // workbox 3.x 开始是将 workbox 核心 lib 放在 CDN 维护
 // 当然也可以挪到自己的 CDN 维护
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
