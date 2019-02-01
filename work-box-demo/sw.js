b467586e628152e6d0c5013d2ab44b9d9adf6e05
anderson <deveil2825@163.com>
2019-02-01 15:56:00 +0800

    修复importScriptjs问题
    修复importScriptjs问题

--- work-box-demo/sw.js
+++ work-box-demo/sw.js
@@ -1,11 +1,13 @@
 console.log('hello from sw.js');
 // workbox 3.x 开始是将 workbox 核心 lib 放在 CDN 维护
 // 当然也可以挪到自己的 CDN 维护
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+if('function' === typeof importScripts){
+	importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+}
 
 if (workbox) {
-    console.log(`Yay! workbox is loaded 🎉`);
+    console.log('Yay! workbox is loaded ');
 }
 else {
-    console.log(`Boo! workbox didn't load 😬`);
+    console.log('Boo! workbox didn\'t load');
 }
