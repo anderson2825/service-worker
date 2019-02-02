f9adc45f8ba902dd2b4f0f465c45ad3d7c4ca3b6
anderson <deveil2825@163.com>
2019-02-02 10:20:41 +0800

    测试service-demo
    测试service-demo

--- service-demo/sw.js
+++ service-demo/sw.js
@@ -1,30 +1,41 @@
-	this.addEventListener('install',function (event){
-		event.waitUntil(
-			caches.open('my-test-cache-v1').then(function (cache){
-				return cache.addAll([
-					'/service-demo/',
-					'/service-demo/index.html',
-	                '/service-demo/main.css',
-	                '/service-demo/main.js',
-	                '/service-demo/images.jpg'
-				]);
-			})		
-		);
-	});
-	this.addEventListener('fetch',function (event) {
-		event.respondWith(
-			caches.match(event.request).then(function (response){
-				if(response !== undefined) {
-					return response;
-				}else{
-					return fetch(event.request).then(function (response){
-	 				 	var responseClone = response.clone();
-	 					caches.open('my-test-cache-v1').then(function (cache) {
-	 						cache.put(event.request,responseClone);
-	 					})
- 						return response;
-					})	
- 				}
- 			});
-		)	
-	});
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/serivce-demo/',
+        '/serivce-demo/index.html',
+        '/serivce-demo/style.css',
+        '/serivce-demo/app.js',
+        '/serivce-demo/image-list.js',
+        '/serivce-demo/images_pro.jpg',
+        '/serivce-demo/images/a1.jpg',
+        '/serivce-demo/images/a2.jpg',
+        '/serivce-demo/images/a3.jpg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/service-demo/images/a3.jpg');
+      });
+    }
+  }));
+});
