16f28593ac76503ecc9cb6d2763200636bab051c
anderson <deveil2825@163.com>
2019-02-02 10:25:51 +0800

    修改图片路径！
    修改图片路径！

--- service-demo/image-list.js
+++ service-demo/image-list.js
@@ -5,21 +5,21 @@ var Gallery = { 'images' : [
   {
     'name'  : 'Darth Vader',
     'alt' : 'A Black Clad warrior lego toy',
-    'url': 'gallery/myLittleVader.jpg',
+    'url': 'images/a1.jpg',
     'credit': '<a href="https://www.flickr.com/photos/legofenris/">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
   },
 
   {
     'name'  : 'Snow Troopers',
     'alt' : 'Two lego solders in white outfits walking across an icy plain',
-    'url': 'gallery/snowTroopers.jpg',
+    'url': 'images/a2.jpg',
     'credit': '<a href="https://www.flickr.com/photos/legofenris/">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
   },
 
   {
     'name'  : 'Bounty Hunters',
     'alt' : 'A group of bounty hunters meeting, aliens and humans in costumes.',
-    'url': 'gallery/bountyHunters.jpg',
+    'url': 'images/a3.jpg',
     'credit': '<a href="https://www.flickr.com/photos/legofenris/">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
   },
   
