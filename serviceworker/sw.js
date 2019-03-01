c128e5712c333bc1c9221f3e21de031ee58a6780
anderson <deveil2825@163.com>
2019-03-01 17:22:47 +0800

    Update sw.js

--- serviceworker/sw.js
+++ serviceworker/sw.js
@@ -1,1 +1,9 @@
 console.log("hello form sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.3/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! workbox is loaded 🎉`);
+}
+else {
+    console.log(`Boo! workbox didn't load 😬`);
+}
