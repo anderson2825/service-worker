2ea8aafd90309580caa308e4164fce7acc3070fd
anderson <deveil2825@163.com>
2019-03-01 18:32:26 +0800

    workerbox4.0
    workerbox4.0

--- serviceworker/sw.js
+++ serviceworker/sw.js
@@ -1,5 +1,5 @@
 console.log("hello form sw.js");
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.3/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js');
 
 if (workbox) {
     console.log(`Yay! workbox is loaded 🎉`);
