142771aa805a23074642dab40e5de90679be011e
anderson <deveil2825@163.com>
2019-04-02 11:06:32 +0800

    Update sw.js

--- service-worker/sw.js
+++ service-worker/sw.js
@@ -1,6 +1,6 @@
 var  precache = 'precache-v1';
-var  urlsTocache = ['/','/style/main.css','/script/main.js'];
-self.addEventListener('install',function(event) {
+var  urlsTocache = ['','style/main.css','script/main.js'];
+self.addEventListener('install',function(event) { 
 	event.waitUntil(
 		caches.open(precache).then(function(cache) {
 			console.log('opened cache');
