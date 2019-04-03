615a120f8b36ba11c86c346df2a918d1286fc3a6
anderson <deveil2825@163.com>
2019-04-03 16:32:12 +0800

    Update sw.js

--- service-install/sw.js
+++ service-install/sw.js
@@ -1,52 +1,49 @@
-var version='v1';
-self.addEventListener('install',function(event) {
-	event.waitUntil(
-		caches.open(version).then(function(cache){
-	   return cache.addAll([
-				'./en/',
-				'./en/image/ban1.jpg',
-				'./en/image/ban2.jpg',
-				'./en/image/ban3.jpg',
-				'./en/script/main.js',
-				'./en/style/main.css'
-			]);
-		})
-	)
-});
-
-// 缓存更新
-self.addEventListener('activate', function(event) {
+var  precache = 'precache-v1';
+var  urlsTocache = ['','en/image/ban1.jpg','en/image/ban2.jpg','en/image/ban3.jpg'];
+self.addEventListener('install',function(event) { 
   event.waitUntil(
-  caches.keys().then(function(cacheNames) {
-      console.log("activate");
+    caches.open(precache).then(function(cache) {
+      console.log('opened cache');
+      return cache.addAll(urlsTocache);
+    })
+  )
+})
+
+self.addEventListener('fetch',function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if(response){
+        return response;
+      }
+      console.log(event.response);
+      return fetch(event.request).then(function(response){
+        if(!response || response.status !=200 || response.type!== 'basic'){
+          return response;
+        }
+        var responseToCache = response.clone();
+
+        caches.open(precache).then(function(cache){
+          cache.put(event.request,responseToCache);
+        })
+        return response;
+      })
+    })
+  )
+})
+
+self.addEventListener('activate',function(event){
+  var cacheWhiteList = ['precache-v1','precache-v2'];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames){
+      console.log(cacheNames);
       return Promise.all(
-        cacheNames.map(function(cacheName) {
-          // 如果当前版本和缓存版本不一致
-          if (cacheName !== version) {
-            return caches.delete(cacheName);
+        cacheNames.map(function(cacheName){
+          if(cacheWhiteList.indexOf(cacheName)=== -1){
+            return cache.delete(cacheName);
           }
         })
-      );
-    })
-  );
-});
+      )
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response){
-      if(response !== undefined){
-        return response;
-      }else{
-        return fetch(event.request).then(function(response){
-          var responseClone = response.clone();
-          caches.open(version).then(function(cache){
-            cache.put(event.request,responseClone);
-          })
-          return response;
-        }).catch(function(){
-          return caches.match('./en/image/ban3.jpg');
-        })
-      }
     })
-  );
-});
+  )
+})
