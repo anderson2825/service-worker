1deca51018a729f93ddb32954a1796fc03405c3f
anderson <deveil2825@163.com>
2019-04-03 15:26:14 +0800

    Update sw.js

--- service-install/sw.js
+++ service-install/sw.js
@@ -1,7 +1,7 @@
 var version='v1';
 self.addEventListener('install',function(event) {
 	event.waitUntil(
-		caches.open('version').then(function(cache){
+		caches.open(version).then(function(cache){
 			cache.addAll([
 				'/en/',
 				'/en/image/ban1.jpg',
@@ -23,9 +23,26 @@ self.addEventListener('install',function(event) {
 	)
 });
 
+// 缓存更新
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+  caches.keys().then(function(cacheNames) {
+      console.log("activate");
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          // 如果当前版本和缓存版本不一致
+          if (cacheName !== version) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('version').then(function(cache) {
+    caches.open(version).then(function(cache) {
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function(response) {
           cache.put(event.request, response.clone());
