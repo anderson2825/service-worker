26b6b24ecb0cb3e96ca5c044873b28efd392e429
anderson <deveil2825@163.com>
2019-04-03 15:48:01 +0800

    Update sw.js

--- service-install/sw.js
+++ service-install/sw.js
@@ -2,16 +2,7 @@ var version='v1';
 self.addEventListener('install',function(event) {
 	event.waitUntil(
 		caches.open(version).then(function(cache){
-			cache.addAll([
-				'/en/',
-				'/en/image/ban1.jpg',
-				'/en/image/ban2.jpg',
-				'/en/image/ban3.jpg',
-				'/en/script/main.js',
-				'/en/style/main.css'
-			]);
-
-			return cache.addAll([
+	   return cache.addAll([
 				'/en/',
 				'/en/image/ban1.jpg',
 				'/en/image/ban2.jpg',
@@ -42,13 +33,20 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open(version).then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
+    caches.match(event.request).then(function(response){
+      if(response !=== undefined){
+        return response;
+      }else{
+        return fetch(event.request).then(function(response){
+          var responseClone = response.clone();
+          caches.open(version).then(function(cache){
+            cache.put(event.request,responseClone);
+          })
           return response;
-        });
-      });
+        }).catch(function(){
+          return caches.match('en/image/ban3.jpg');
+        })
+      }
     })
   );
 });
