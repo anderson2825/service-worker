a93109efd85335a4761d2fcbe6ee5fc4c0a9e83c
anderson <deveil2825@163.com>
2019-04-03 15:12:39 +0800

    sw3

---
+++ service-install/sw.js
@@ -0,0 +1,37 @@
+var version='v1'
+self.addEventListener('install',function(event) {
+	event.waitUntill(
+		caches.open(version).then(function(cache){
+			cache.addAll([
+				'/en/',
+				'/en/image/ban1.jpg',
+				'/en/image/ban2.jpg',
+				'/en/image/ban3.jpg',
+				'/en/script/main.js',
+				'/en/style/main.css'
+			]);
+
+			return cache.addAll([
+				'/en/',
+				'/en/image/ban1.jpg',
+				'/en/image/ban2.jpg',
+				'/en/image/ban3.jpg',
+				'/en/script/main.js',
+				'/en/style/main.css'
+			]);
+		})
+	)
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('version').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
