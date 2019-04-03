2db934387624017c5d47e4119eba6fa0975fac48
anderson <deveil2825@163.com>
2019-04-03 16:16:10 +0800

    Update sw.js

--- service-install/sw.js
+++ service-install/sw.js
@@ -3,12 +3,12 @@ self.addEventListener('install',function(event) {
 	event.waitUntil(
 		caches.open(version).then(function(cache){
 	   return cache.addAll([
-				'/en/',
-				'/en/image/ban1.jpg',
-				'/en/image/ban2.jpg',
-				'/en/image/ban3.jpg',
-				'/en/script/main.js',
-				'/en/style/main.css'
+				'/service-install/',
+				'/service-install/en/image/ban1.jpg',
+				'/service-install/en/image/ban2.jpg',
+				'/service-install/en/image/ban3.jpg',
+				'/service-install/en/script/main.js',
+				'/service-install/en/style/main.css'
 			]);
 		})
 	)
