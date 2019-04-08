c70a3e0d85921a303e8c93eb483c61e647e9143b
anderson <deveil2825@163.com>
2019-04-08 10:37:02 +0800

    Update sw.js

--- servicewoker/sw.js
+++ servicewoker/sw.js
@@ -1,25 +1,42 @@
-this.addEventListener('intall',function (event) {
-	event.waitUntil(
-		caches.open('v1').then(function (cache) {
-			return cache.addAll([
-				'/',
-				'/index.html',
-				'/style.css',
-				'/worker.js'		
-			])
-	 	})		
-	)
-});
+var cache_version = 1;
+var current_caches = {
+	font: 'font-cache-v' + cache_version;
+};
 
-this.addEventListener('fetch',function (event) {
-	event.respondWith(
-		caches.match(event.request).then(function () {
-			return fetch(event.request).then(function (response) {
-				return caches.open('v1').then(function (cache) {
-					cache.put(event.request,response.clone());
-					return response;
-				});
-		 	});
-		})	
+self.addEventListener('activate',function(event){
+	var expecatedCacheNames = Object.keys(current_caches).map(function(key){
+		return current_caches[key];
+	})
+
+	event.waitUntil(
+		caches.keys().then(function(cacheNames)){
+			return Promise.all(
+				cacheNames.map(function(cacheName){
+					if(expecatedCacheNames.indexOf(cacheName)== -1){
+						console.log('Deleting out of date cache',cacheName);
+						return caches.delete(cacheName);
+					}
+				})
+			)
+		}
 	)
+})
+
+
+self.addEventListener('fetch',function (event) {
+	console.log('handling fetch event for',event.request.url);
+	event.respondWith(
+		caches.open(current_caches['font']).then(function(){
+			return cache.match(event.request).then(function(response){
+				if(response){
+					console.log('found response in cache:',response);
+					return response;
+				}
+
+			}).catch(function(err){
+				console.error('error in fetch handler:',err);
+				throw err;
+			})
+		})
+ 	)
 })
