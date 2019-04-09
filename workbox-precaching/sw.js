1e68fa39e4629d0c0e13bdb9a3c351403d5d3208
anderson <deveil2825@163.com>
2019-04-09 17:29:28 +0800

    Update sw.js

--- workbox-precaching/sw.js
+++ workbox-precaching/sw.js
@@ -9,7 +9,7 @@ if (workbox) {
 workbox.precaching.precacheAndRoute([
 	'js/index.js',
 	'css/index.css',
-	{url:'index.html?key2=2',revision:'385554'},
+	{url:'index.html?key2=2',revision:'258541'},
  ],{
 	ignoreURLParametersMatching:[/.*/]   //删除.html文件后缀
 });
