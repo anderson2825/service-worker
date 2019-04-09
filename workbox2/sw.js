03464cee759d09723122e5185dd4416a621ecded
anderson <deveil2825@163.com>
2019-04-09 14:03:24 +0800

    Update sw.js

--- workbox2/sw.js
+++ workbox2/sw.js
@@ -24,7 +24,7 @@ workbox.precaching.precacheAndRoute([
 ])
 workbox.routing.registerRoute(
 	matchFun(),
-	new RegExp('.*\.js'),
+	/*new RegExp('.*\.js'),*/
 	new workbox.strategies.NetworkFirst({
 		cacheName:'my-js-cache',
 	})
